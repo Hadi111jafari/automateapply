2c53fe5c061cc317c43cef12f14a5e859b2b1d98
Hadi Jafari <abdulhadijafari2015@gmail.com>
2026-08-09 22:05:04 +0430

    feat: add resume page component with workspace layout

---
+++ app/resume/page.tsx
@@ -0,0 +1,9 @@
+import { Resume } from '@/components/workspace/resume';
+import { WorkspacePage } from '@/components/workspace/workspace-shell';
+export default function Page() {
+  return (
+    <WorkspacePage>
+      <Resume />
+    </WorkspacePage>
+  );
+}
